3b045bdfbdf0bf244f0c48da32ca4275099d686d
George Le Van Thong <thonglevan131@gmail.com>
2022-11-17 22:28:23 +0700

    [WIP] modules + deployment

--- modules/location-producer/main.py
+++ modules/location-producer/main.py
@@ -6,7 +6,7 @@ from concurrent import futures
 
 from utils import publish_location
 
-class LocationIngesterServicer(location_pb2_grpc.LocationServiceServicer):
+class LocationProducerServicer(location_pb2_grpc.LocationServiceServicer):
     def Create(self, request, context):
         payload = {
             "person_id": int(request.person_id),
@@ -21,7 +21,7 @@ class LocationIngesterServicer(location_pb2_grpc.LocationServiceServicer):
 
 # Intiialize gRPC server
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-location_pb2_grpc.add_LocationServiceServicer_to_server(LocationIngesterServicer(), server)
+location_pb2_grpc.add_LocationServiceServicer_to_server(LocationProducerServicer(), server)
 
 print("Starting gRPC server on port 5005...")
 server.add_insecure_port("[::]:5005")
@@ -29,8 +29,3 @@ server.start()
 
 # Keep thread alive
 server.wait_for_termination()
-# try:
-#     while True:
-#         time.sleep(86400)
-# except KeyboardInterrupt:
-#     server.stop(0)
--- modules/location-producer/utils.py
+++ modules/location-producer/utils.py
@@ -10,21 +10,21 @@ KAFKA_SERVER = os.environ["KAFKA_SERVER"]
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("udaconnect-location-ingester-service")
+logger = logging.getLogger("udaconnect-location-producer-service")
 
 # Create the kafka producer
 producer = KafkaProducer(bootstrap_servers=[KAFKA_SERVER])
 
 
 def publish_location(location_data):
-    print(f"Data to be sent to kafka: {location_data}")
+    print(f"Sending data to Kafka:{location_data}")
 
     encoded_data = json.dumps(location_data).encode('utf-8')
-    print(f"Data to be sent to kafka: {encoded_data}")
+    print(f"Sending data to Kafka:{encoded_data}")
     producer.send(TOPIC_NAME, encoded_data)
     producer.flush()
 
-    logger.info(f"Published location data {location_data} to kafka successfully.")
+    logger.info(f"Successfully uploaded location data {location_data} to Kafka.")
 
 
 
